61dded7aeab07cf25eacd7bb7258c8fcf03d34ee
Kd125 <kashdangar1250@gmail.com>
2023-03-12 23:57:31 -0500

    New commit

--- src/numbers/index.js
+++ src/numbers/index.js
@@ -1,20 +1,28 @@
 const isEven = (num) => {
-  // write code for numbers.isEven
+  if (num % 2 == 0) {
+    return true;
+  }
+};
 
-}
+let myArray = [4, 2, 2, 9, 1];
 
 const sum = (arr) => {
-  // write code for numbers.sum
+  let sumNumbers;
+  for (let i = 0; i < arr.length; i++) {
+    sumNumbers + arr[i];
+    console.log("Current value is: " + arr[i]);
+  }
+  console.log(sumNumbers);
+};
 
-}
+sum(myArray);
 
 const comboSum = (arr, sum) => {
   // write code for numbers.comboSum
-
-}
+};
 
 module.exports = {
   isEven,
   sum,
-  comboSum
-}
+  comboSum,
+};
